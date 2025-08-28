63b467258dfc29203e0956ad7c45a82145b2b819
Pulkit Garg <pulkitgargbnl@gmail.com>
2025-08-29 01:15:39 +0530

    chore: add doxygen-style comments to main file and checkstyle using cpplint

--- src/main.c
+++ src/main.c
@@ -1,20 +1,32 @@
+// Copyright 2025 Pulkit Garg
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "createfile.h"
-#include "writefile.h"
-#include "appendfile.h"
-#include "copyfile.h"
-#include "readfile.h"
-#include "deletefile.h"
-#include "renamefile.h"
-#include "movefile.h"
-#include "createdir.h"
-#include "deletedir.h"
-#include "listdir.h"
+#include "./createfile.h"
+#include "./writefile.h"
+#include "./appendfile.h"
+#include "./copyfile.h"
+#include "./readfile.h"
+#include "./deletefile.h"
+#include "./renamefile.h"
+#include "./movefile.h"
+#include "./createdir.h"
+#include "./deletedir.h"
+#include "./listdir.h"
 
+/**
+ * @brief Entry point of the Mini File Management System.
+ *
+ * Provides a menu-driven interface to perform basic file and directory
+ * operations such as create, write, append, copy, read, delete, rename,
+ * move, create directory, delete directory, and list directory contents.
+ *
+ * @return 0 on successful execution.
+ */
 int main() {
     int choice;
-    char file1[256], file2[256];
+    char file1[256];
+    char file2[256];
 
     while (1) {
         printf("\n===== Mini File Management System =====\n");
@@ -36,63 +48,63 @@ int main() {
         switch (choice) {
             case 1:
                 printf("Enter filename: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 createFile(file1);
                 break;
             case 2:
                 printf("Enter filename: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 writeFile(file1);
                 break;
             case 3:
                 printf("Enter filename: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 appendFile(file1);
                 break;
             case 4:
                 printf("Enter source filename: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 printf("Enter destination filename: ");
-                scanf("%s", file2);
+                scanf("%255s", file2);
                 copyFile(file1, file2);
                 break;
             case 5:
                 printf("Enter filename: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 readFile(file1);
                 break;
             case 6:
                 printf("Enter filename to delete: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 deleteFile(file1);
                 break;
             case 7:
                 printf("Enter current filename: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 printf("Enter new filename: ");
-                scanf("%s", file2);
+                scanf("%255s", file2);
                 renameFile(file1, file2);
                 break;
             case 8:
                 printf("Enter source file path: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 printf("Enter destination file path: ");
-                scanf("%s", file2);
+                scanf("%255s", file2);
                 moveFile(file1, file2);
                 break;
             case 9:
                 printf("Enter directory name to create: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 createDirectory(file1);
                 break;
             case 10:
                 printf("Enter directory name to delete: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 deleteDirectory(file1);
                 break;
             case 11:
                 printf("Enter directory path to list: ");
-                scanf("%s", file1);
+                scanf("%255s", file1);
                 listDirectory(file1);
                 break;
             case 12:
@@ -102,5 +114,6 @@ int main() {
                 printf("Invalid choice, try again.\n");
         }
     }
-}
 
+    return 0;
+}
